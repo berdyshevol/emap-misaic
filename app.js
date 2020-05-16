bc54a0da97bd33934469db687b0bde4e3e508eb7
petru6e4ka <petru6e4ka@gmail.com>
2020-05-16 12:19:59 +0300

    testing grid areas
    * random x y
    * random width height
    * matrix

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
 const root = document.getElementById('root');
+const field = [];
+const emptyCell = 0;
+const max= 10;
+const min = 0;
 
+root.classList.add('mosaic-container');
+
+for(let i = 0; i < max; i++) {
+    const row = [];
+    for(let j = 0; j < max; j++) {
+        row.push(emptyCell);
+    }
+    field.push(row);
+}
+console.log(field.length, field[0].length);
+
+function randomPosition() {
+    return Math.floor(Math.random() * max);
+}
+
+function randomSize() {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function placeRect(x, y, width, height) {
+    const elem = document.createElement('div');
+    elem.classList.add('mosaic-element');
+    root.append(elem);
+    elem.style.gridRow = `${x + 1} / ${width}`;
+    elem.style.gridColumn = `${y + 1} / ${height}`;
+}
+
+function randomRect(){
+    let width = randomSize(); 
+    let height = randomSize();
+    let x = randomPosition();
+    let y = randomPosition();
+    console.log(x, width , y, height);
+    placeRect(x, y, width, height);
+}
+   
+randomRect();
